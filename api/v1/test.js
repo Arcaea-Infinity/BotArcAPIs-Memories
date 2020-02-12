bda6050f7996320d90ce34c94fbbb64f6cd4d614
CirnoBakaBOT <2-CirnoBakaBOT@users.noreply.git.moezsoft.com>
2020-02-12 10:25:43 +0800

    updated test.js

--- api/v1/test.js
+++ api/v1/test.js
@@ -1,9 +1,23 @@
 // filename : /v1/test.js
 // author   : CirnoBakaBOT
 // date     : 02/10/2020
-// comment  : test
+// comment  : test api
 
-export default function () {
-    return "test";
+import Utils from 'Utils';
+
+export default async function (argument) {
+
+    // initialize response data
+    let _response_code = 200;
+    let _response_message = 'ok';
+    let _response_data_template = {
+        'test': null
+    };
+
+    // fill the template
+    _response_data_template.test = '(｡･∀･)ﾉﾞ嗨';
+
+    // make response
+    return Utils.MakeApiObject(_response_code, _response_message, _response_data_template);
 };
 
