4a95cd9da5b5f32e63611beb187c837a012332de
CirnoBakaBOT <2-CirnoBakaBOT@users.noreply.git.moezsoft.com>
2020-02-12 10:26:52 +0800

    added v1 api checkupdate

--- api/v1/__main__.js
+++ api/v1/__main__.js
@@ -6,13 +6,15 @@
 import __test__ from "./test.js";
 import __rank__ from "./rank.js";
 import __best30__ from "./best30.js";
-import __userecent__ from "./userecent.js";
 import __userinfo__ from "./userinfo.js";
+import __userecent__ from "./userecent.js";
+import __checkupdate__ from "./checkupdate.js";
 
 export default function BotArcAPI() {
     this.test = __test__;
     this.rank = __rank__;
     this.best30 = __best30__;
-    this.userecent = __userecent__;
     this.userinfo = __userinfo__;
+    this.userecent = __userecent__;
+    this.checkupdate = __checkupdate__;
 };
---
+++ api/v1/checkupdate.js
@@ -0,0 +1,43 @@
+// filename : /v1/checkupdate.js
+// author   : CirnoBakaBOT
+// date     : 02/10/2020
+// comment  : checkupdate
+
+import Utils from 'Utils';
+
+export default async function (argument) {
+
+    // initialize response data
+    let _response_code = 200;
+    let _response_message = 'ok';
+    let _response_data_template = {
+        'version': null,
+        'download_link': null
+    };
+
+    // build http request
+    let _remote_request_object =
+        new Request(
+            'https://www.fantasyroom.cn/api/v1/arcver.php', {
+            method: 'GET',
+            headers: {
+                'user-agent': 'BotArcAPI'
+            }
+        });
+    const _remote_response_data = await fetch(_remote_request_object);
+    const _json_root = await _remote_response_data.json();
+
+    console.log(_json_root);
+
+    // fill the template
+    if (_json_root instanceof Object) {
+        if (_json_root.res == 'success') {
+            _response_data_template.version = _json_root.contents.android.version;
+            _response_data_template.download_link = _json_root.contents.android.apk_dl_link;
+        }
+    }
+
+    // make response
+    return Utils.MakeApiObject(_response_code, _response_message, _response_data_template);
+};
+
