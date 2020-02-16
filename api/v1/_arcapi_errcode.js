ef461c8949c732d2788bd6d054fc965ec3d7ce56
CirnoBakaBOT <2-CirnoBakaBOT@users.noreply.git.moezsoft.com>
2020-02-16 15:33:55 +0800

    added arcapi errcode definitions

---
+++ api/v1/_arcapi_errcode.js
@@ -0,0 +1,22 @@
+// filename : /v1/_arcapi_errcode.js
+// author   : CirnoBakaBOT
+// date     : 02/16/2020
+// comment  : arcapi errcode definitions
+
+export default function (errcode) {
+
+  const _errcode_table = {
+    002: 'Server is under maintenance',
+    005: 'Incorrect app version',
+    104: 'Incorrect username or password ',
+    106: 'This account is locked',
+    401: 'Server access denied',
+    602: 'Friend existed',
+  };
+
+  try {
+    return _errcode_table[errcode];
+  } catch (e) { return `Unknown error ${errcode}`; }
+
+}
+
