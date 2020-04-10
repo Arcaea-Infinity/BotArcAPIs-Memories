26b43938d6ab68b7e533c91632f7320a892046aa
CirnoBakaBOT <2-CirnoBakaBOT@users.noreply.git.moezsoft.com>
2020-04-10 12:17:00 +0800

    updated auto loader

--- source/__loader__.js
+++ source/__loader__.js
@@ -28,34 +28,35 @@ async function handleRequest(request, response) {
   const _api_arguments = Object.fromEntries(_api_url.searchParams);
   console.log(TAG, _api_path, _api_arguments);
 
-  // try invoke method
   try {
+    // try invoke method
+
     const _api_entry = require(`./publicapi/${_api_path}`);
     const _api_result = await _api_entry(_api_arguments);
 
-    // fill result
     _response_template.status = _api_result.status;
-    _response_template.content = _api_result.content;
-
-  } catch (e) { console.log(e); _response_status = 404; }
+    if (_api_result.status < 0) {
+      // if failed delete content field
+      delete _response_template.content;
+      _response_template.message = _api_result.message;
+    }
+    else {
+      // if success delete message field
+      delete _response_template.message;
+      _response_template.content = _api_result.content;
+    }
+  }
+  catch (e) {
+    _response_status = 404;
+  }
 
   // make response body
   let _http_body = null;
 
-  if (_response_status == 404) {
+  if (_response_status == 404)
     _http_body = 'request path notfound =(:3) z)_';
-  }
-  else {
-
-    // if any errors occurred
-    // should delete content field
-    if (_response_template.status < 0)
-      delete _response_template.content;
-    else
-      delete _response_template.message;
-
+  else
     _http_body = JSON.stringify(_response_template);
-  }
 
   // send json result to client
   response.statusCode = _response_status;
