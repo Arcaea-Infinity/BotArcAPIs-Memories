aa4239fe60f688a0445a0a1803c417a1d6fb11a2
CirnoBakaBOT <2-CirnoBakaBOT@users.noreply.git.moezsoft.com>
2020-04-10 12:16:21 +0800

    updated v1/checkupdate

--- source/publicapi/v1/checkupdate.js
+++ source/publicapi/v1/checkupdate.js
@@ -3,37 +3,47 @@
 // date     : 02/10/2020
 // comment  : checkupdate
 
-import Utils from 'Utils';
+const TAG = 'v1/checkupdate.js';
+const fetch = require('node-fetch');
 
-export default async function (argument) {
-
-  // initialize response data
-  let _response_status = 200;
-  const _response_data_template = {
-    'version': null,
-    'download_link': null
+module.exports = async function (argument) {
+  const _response_template = {
+    'status': null,
+    'message': null,
+    'content': {
+      'version': null,
+      'download_link': null
+    }
   };
 
   // build http request
-  const _remote_request_object =
-    new Request('https://www.fantasyroom.cn/api/v1/arcver.php', {
+  const _remote_request =
+    new fetch.Request('https://www.fantasyroom.cn/api/v1/arcver.php', {
       method: 'GET',
       headers: {
         'User-Agent': 'BotArcAPI'
       }
     });
-  const _remote_response_data = await fetch(_remote_request_object);
-  const _json_root = await _remote_response_data.json();
+  const _remote_response = await fetch(_remote_request);
+  const _json_root = await _remote_response.json();
 
-  // fill the data template
   try {
-    if (_json_root.res == 'success') {
-      _response_data_template.version = _json_root.contents.android.version;
-      _response_data_template.download_link = _json_root.contents.android.apk_dl_link;
-    } else _response_status = 503;
-  } catch (e) { _response_status = 502; }
 
-  // make response
-  return Utils.MakeApiObject(_response_status, _response_data_template);
+    // fill the data template
+    if (_json_root.res == 'success') {
+      _response_template.status = 0;
+      _response_template.content.version = _json_root.contents.android.version;
+      _response_template.content.download_link = _json_root.contents.android.apk_dl_link;
+
+    } else {
+      _response_template.status = -2;
+      _response_template.message = 'fetch latest version failed';
+    }
+  } catch (e) {
+    _response_template.status = -1;
+    _response_template.message = 'remote service unavaliable';
+  }
+
+  return _response_template;
 };
 
