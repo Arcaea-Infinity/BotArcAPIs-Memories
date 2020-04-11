033f318a15b186ee4906eb214e88112604ac0444
CirnoBakaBOT <2-CirnoBakaBOT@users.noreply.git.moezsoft.com>
2020-04-12 03:47:13 +0800

    reimplemented checkupdate

--- source/publicapi/v1/checkupdate.js
+++ source/publicapi/v1/checkupdate.js
@@ -7,9 +7,9 @@ const TAG = 'v1/checkupdate.js';
 
 const fetch = require('node-fetch');
 const Request = require('node-fetch').Request;
-const APIError = require('../../error');
+const APIError = require('../../corefunc/error');
 
-module.exports = async function (argument) {
+module.exports = async function () {
   const _return_template = {
     'status': 0,
     'message': null,
@@ -19,35 +19,37 @@ module.exports = async function (argument) {
     }
   };
 
-  // build http request
-  const _remote_request =
-    new Request('https://www.fantasyroom.cn/api/v1/arcver.php', {
-      method: 'GET',
-      headers: {
-        'User-Agent': 'BotArcAPI'
-      }
-    });
-
   try {
-    let _remote_response = null;
-    let _json_root = null;
 
-    // try to request origin service
-    try {
-      _remote_response = await fetch(_remote_request);
-      _json_root = await _remote_response.json();
-    } catch (e) {
-      throw new APIError(-1, 'remote service unavaliable');
-    }
+    // construct remote request
+    const _promise_remote =
+      new Request('https://www.fantasyroom.cn/api/v1/arcver.php', {
+        method: 'GET',
+        headers: {
+          'User-Agent': 'BotArcAPI'
+        }
+      });
 
-    // fill the data template
-    if (_json_root.res != 'success')
-      throw new APIError(-2, 'fetch latest version failed');
-    _return_template.content.version = _json_root.contents.android.version;
-    _return_template.content.download_link = _json_root.contents.android.apk_dl_link;
+    // request remote service
+    await fetch(_promise_remote)
+      .then((response) => {
+        // parse json if success
+        if (response.status === 200)
+          return response.json();
+        throw new APIError(-1, 'remote service now is unavailable');
+      })
+      .then((jsonroot) => {
+        // check result if success
+        if (jsonroot.res === 'success') {
+          _return_template.content.version = jsonroot.contents.android.version;
+          _return_template.content.download_link = jsonroot.contents.android.apk_dl_link;
+        }
+        else throw new APIError(-2, 'request remote service failed');
+      })
+      .catch((e) => { throw new APIError(-3, 'request timeout'); });
 
   } catch (e) {
-    // return errcode and errmessage
+    syslog.e(e.notyify);
     _return_template.status = e.status;
     _return_template.message = e.notify;
   }
