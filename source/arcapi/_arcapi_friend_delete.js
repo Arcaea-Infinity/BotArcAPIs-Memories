19bde189289a2ba42015c1243240fae57359ff48
CirnoBakaBOT <2-CirnoBakaBOT@users.noreply.git.moezsoft.com>
2020-04-13 00:38:43 +0800

    updated arcapi friend delete

--- source/arcapi/_arcapi_friend_delete.js
+++ source/arcapi/_arcapi_friend_delete.js
@@ -8,26 +8,26 @@ const TAG = 'arcapi/_arcapi_friend_delete.js';
 const arcfetch = require('../corefunc/arcfetch');
 const ArcAPIRequest = arcfetch.ArcAPIRequest;
 
-module.exports = async function (arc_account, user_id) {
+module.exports = async function (account, userid) {
   const _return_template = {
     success: false,
-    friend_list: null
+    friends: null
   };
 
   try {
 
     // construct remote request
     const _remote_request =
-      new ArcAPIRequest('POST', `friend/me/delete`, {
-        usertoken: arc_account.token,
-        postdata: new URLSearchParams({ 'friend_id': user_id })
+      new ArcAPIRequest('POST', 'friend/me/delete', {
+        usertoken: account.token,
+        postdata: new URLSearchParams({ 'friend_id': userid })
       });
 
     // send request
     await arcfetch(_remote_request)
       .then((root) => {
         _return_template.success = true;
-        _return_template.friend_list = root.value.friends;
+        _return_template.friends = root.value.friends;
       })
       .catch((e) => { throw e; })
 
