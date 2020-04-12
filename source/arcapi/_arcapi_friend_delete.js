7f46ff51c6809992f4705aff7acf6b36dc35608e
CirnoBakaBOT <2-CirnoBakaBOT@users.noreply.git.moezsoft.com>
2020-04-12 09:41:02 +0800

    reimplemented arcapi delete friend

--- source/arcapi/_arcapi_friend_delete.js
+++ source/arcapi/_arcapi_friend_delete.js
@@ -1,46 +1,37 @@
-// filename : /source/arcapi/_arcapi_friend_delete.js
+// filename : arcapi/_arcapi_friend_delete.js
 // author   : CirnoBakaBOT
-// date     : 04/09/2020
+// date     : 04/12/2020
 // comment  : delete friend
 
+const TAG = 'arcapi/_arcapi_friend_delete.js';
+
+const arcfetch = require('../corefunc/arcfetch');
+const ArcAPIRequest = arcfetch.ArcAPIRequest;
+
 module.exports = async function (arc_account, user_id) {
-  const TAG = '_arcapi_friend_delete.js';
   const _return_template = {
     success: false,
     arc_friendlist: null
   };
 
-  // request origin arcapi
-  const fetch = require('node-fetch');
-  const _remote_request =
-    new fetch.Request(`https://arcapi.lowiro.com/${ARCAPI_VERSION}/friend/me/delete`, {
-      method: 'POST',
-      headers: {
-        'Accept-Encoding': 'identity',
-        'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
-        'Platform': 'android',
-        'Authorization': `Bearer ${arc_account.token}`,
-        'AppVersion': ARCAPI_APPVERSION,
-        'User-Agent': ARCAPI_USERAGENT,
-        'Host': 'arcapi.lowiro.com',
-        'Connection': 'Keep-Alive'
-      },
-      body: new URLSearchParams({
-        'friend_id': user_id
-      })
-    });
-  const _remote_response_data = await fetch(_remote_request);
-
-  // check for origin arcapi data
   try {
-    const _json_root = await _remote_response_data.json();
-    console.log(TAG, _json_root);
 
-    if (_json_root.success) {
-      _return_template.success = true;
-      _return_template.arc_friendlist = _json_root.value.friends;
-    }
-  } catch (e) { console.log(TAG, e); }
+    // construct remote request
+    const _remote_request =
+      new ArcAPIRequest('POST', `friend/me/delete`, {
+        usertoken: arc_account.token,
+        postdata: new URLSearchParams({ 'friend_id': user_id })
+      });
+
+    // send request
+    await arcfetch(_remote_request)
+      .then((root) => {
+        _return_template.success = true;
+        _return_template.arc_friendlist = root.value.friends;
+      })
+      .catch((e) => { throw e; })
+
+  } catch (e) { syslog.e(TAG, e); }
 
   return _return_template;
 }
