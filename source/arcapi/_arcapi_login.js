526944361c05e42d09ba0873411b1f14598ea770
CirnoBakaBOT <2-CirnoBakaBOT@users.noreply.git.moezsoft.com>
2020-04-13 00:32:29 +0800

    updated arcapi login

--- source/arcapi/_arcapi_login.js
+++ source/arcapi/_arcapi_login.js
@@ -5,9 +5,8 @@
 
 const TAG = 'arcapi/_arcapi_login.js';
 
-const btoa = require('btoa');
-const fetch = require('node-fetch');
-const Request = fetch.Request;
+const arcfetch = require('../corefunc/arcfetch');
+const ArcAPIRequest = arcfetch.ArcAPIRequest;
 
 module.exports = async (name, password, deviceid) => {
   const _return_template = {
@@ -18,44 +17,23 @@ module.exports = async (name, password, deviceid) => {
   try {
 
     // construct remote request
-    const _promise_remote =
-      new Request(`https://arcapi.lowiro.com/${ARCAPI_VERSION}/auth/login`, {
-        method: 'POST',
-        headers: {
-          'Accept-Encoding': 'identity',
-          'Authorization': `Basic ${btoa(`${name}:${password}`)}`,
-          'DeviceId': deviceid,
-          'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
-          'AppVersion': ARCAPI_APPVERSION,
-          'User-Agent': ARCAPI_USERAGENT,
-          'Host': 'arcapi.lowiro.com',
-          'Connection': 'Keep-Alive'
-        },
-        body: new URLSearchParams({
-          'grant_type': 'client_credentials'
-        })
+    const _remote_request =
+      new ArcAPIRequest('POST', `auth/login`, {
+        username: name,
+        userpwd: password,
+        deviceid: deviceid,
+        postdata: new URLSearchParams({ 'grant_type': 'client_credentials' })
       });
 
-    // request origin arcapi
-    await fetch(_promise_remote)
-      .then((response) => {
-        // parse json if success
-        if (response.status === 200)
-          return response.json();
-        throw new Error('arcapi now is unavailable');
+    // send request
+    await arcfetch(_remote_request)
+      .then((root) => {
+        _return_template.success = true;
+        _return_template.access_token = root.access_token;
       })
-      .then((jsonroot) => {
-        // check result if success
-        if (jsonroot.success) {
-          _return_template.success = true;
-          _return_template.access_token = jsonroot.access_token;
-        }
-        else throw new Error('request origin arcapi failed');
-      });
+      .catch((e) => { throw e; })
 
-  } catch (e) {
-    syslog.e(TAG, e);
-  }
+  } catch (e) { syslog.e(TAG, e); }
 
   return _return_template;
 }
