78f1503c9ec7e7500433a6425a18d5bb031db958
CirnoBakaBOT <2-CirnoBakaBOT@users.noreply.git.moezsoft.com>
2020-04-13 00:47:35 +0800

    updated arcapi rank me

--- source/arcapi/_arcapi_rank_me.js
+++ source/arcapi/_arcapi_rank_me.js
@@ -1,47 +1,40 @@
-// filename : /source/publicapi/_arcapi_rank_me.js
+// filename : arcapi/_arcapi_rank_me.js
 // author   : CirnoBakaBOT
-// date     : 04/09/2020
+// date     : 04/13/2020
 
-module.exports = async function (arc_account, song_id, difficulty, start = 0, limit = 10) {
-  const TAG = '_arcapi_rank_me.js';
+const TAG = '_arcapi_rank_me.js';
+
+const arcfetch = require('../corefunc/arcfetch');
+const ArcAPIRequest = arcfetch.ArcAPIRequest;
+
+module.exports = async function (account, songid, difficulty, start = 0, limit = 10) {
   const _return_template = {
     success: false,
-    arc_ranklist: null
+    ranklist: null
   };
 
-  // request origin arcapi
-  const fetch = require('node-fetch');
-  const _remote_request =
-    new fetch.Request(`https://arcapi.lowiro.com/${ARCAPI_VERSION}/score/song/me?` +
-      new URLSearchParams({
-        'song_id': song_id,
-        'difficulty': difficulty,
-        'start': start,
-        'limit': limit
-      }), {
-      method: 'GET',
-      headers: {
-        'Accept-Encoding': 'identity',
-        'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
-        'Authorization': `Bearer ${arc_account.token}`,
-        'Platform': 'android',
-        'AppVersion': ARCAPI_APPVERSION,
-        'User-Agent': ARCAPI_USERAGENT,
-        'Host': 'arcapi.lowiro.com',
-        'Connection': 'Keep-Alive'
-      }
-    });
-  const _remote_response_data = await fetch(_remote_request);
-
-  // check for origin arcapi data
   try {
-    const _json_root = await _remote_response_data.json();
-    console.log(TAG, _json_root);
 
-    if (_json_root.success) {
-      _return_template.success = true;
-      _return_template.arc_ranklist = _json_root.value;
-    }
+    // construct remote request
+    const _remote_request =
+      new ArcAPIRequest('GET', 'score/song/me?' +
+        new URLSearchParams({
+          'song_id': songid,
+          'difficulty': difficulty,
+          'start': start,
+          'limit': limit
+        }), {
+        usertoken: account.token
+      });
+
+    // send request
+    await arcfetch(_remote_request)
+      .then((root) => {
+        _return_template.success = true;
+        _return_template.ranklist = root.value;
+      })
+      .catch((e) => { throw e; })
+
   } catch (e) { console.log(TAG, e); }
 
   return _return_template;
