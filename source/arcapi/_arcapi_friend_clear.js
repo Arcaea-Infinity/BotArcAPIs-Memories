b007115c9d8d80da296f0077c46c832da2a9d800
CirnoBakaBOT <2-CirnoBakaBOT@users.noreply.git.moezsoft.com>
2020-04-13 00:37:58 +0800

    updated arcapi friend clear

--- source/arcapi/_arcapi_friend_clear.js
+++ source/arcapi/_arcapi_friend_clear.js
@@ -1,26 +1,44 @@
 // filename : /arcapi/_arcapi_friend_clear.js
 // author   : CirnoBakaBOT
 // date     : 04/11/2020
-// comment  : delete all friends from arc account
+// comment  : delete all friends from an arc account
 
 const TAG = '/arcapi/_arcapi_friend_clear.js';
 
 const arcapi_userme = require('./_arcapi_userme');
 const arcapi_friend_delete = require('./_arcapi_friend_delete');
 
-module.exports = async function (arc_account, arc_friendlist = []) {
+module.exports = async (account, friends = []) => {
 
-  // if no friend list pass in, we must request the origin arcapi
-  if (!arc_friendlist) {
-    const _arc_account_info = await arcapi_userme(_arc_account);
+  // we must request the origin arcapi
+  // if no friend list passed in
+  if (!friends.length) {
+    const _return = await arcapi_userme(account);
 
-    // check data valid
-    if (_arc_account_info)
-      arc_friendlist = _arc_account_info.friends;
+    // check data is valid
+    if (_return.success)
+      friends = _return.info.friends;
+    else {
+      syslog.e(TAG, `Error when fetching account information => ${account.name}`);
+      return false;
+    }
   }
 
-  // here we go
-  arc_friendlist.forEach(friend => {
-    await arcapi_friend_delete(arc_account, friend.user_id);
-  });
-}
+  // delete all friends
+  if (friends.length) {
+    try {
+      friends.forEach(v => {
+        const _return = await arcapi_friend_delete(account, v.user_id);
+        if (!_return.success) throw new Error('wtf???');
+      });
+
+    } catch (e) {
+      syslog.e(TAG, `Error when clearing friend list => ${account.name}`);
+      return false;
+    }
+  }
+
+  // clear origin friendlist and return
+  friends = [];
+  return true;
+};
