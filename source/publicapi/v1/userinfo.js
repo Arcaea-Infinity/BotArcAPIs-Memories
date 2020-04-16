4e8c6c06d38398730a9170fd8206fae637b0e0ff
CirnoBakaBOT <2-CirnoBakaBOT@users.noreply.git.moezsoft.com>
2020-04-17 06:15:29 +0800

    reimplemented public api userinfo

--- source/publicapi/v1/userinfo.js
+++ source/publicapi/v1/userinfo.js
@@ -17,75 +17,78 @@ const dbproc_arcplayer_update = require('../../database/_dbproc_arcplayer_update
 module.exports = (argument) => {
   return new Promise(async (resolve, reject) => {
 
-    let _arc_account = null;
-
     try {
 
       // /userinfo?usercode=xxx[&hasrecent=true]
       // check for request arguments
       if (typeof argument.usercode == 'undefined')
-        reject(new APIError(-1, 'invalid argument'));
+        throw new APIError(-1, 'invalid argument');
+
+      let _arc_account = null;
+      let _arc_friends = null;
+      let _arc_friend = null;
 
       // request an arc account
-      await arcapi_account_alloc()
-        .then((account) => { _arc_account = account; })
-        .catch((error) => { reject(new APIError(-2, 'request an arc account from pool failed')); });
+      try {
+        _arc_account = await arcapi_account_alloc();
+      } catch (e) { throw new APIError(-2, 'request an arc account from pool failed'); }
 
-      // clear friend list
-      await arcapi_friend_clear(_arc_account)
-        .catch((error) => { reject(new APIError(-3, 'internal error')); });
+      try {
 
-      // add friend
-      let _arc_friend = null;
-      await arcapi_friend_add(_arc_account, argument.usercode)
-        .then((friends) => {
-          // length must be 1
-          if (friends.length != 1)
-            return reject(new APIError(-4, 'internal error'));
+        // clear friend list
+        try {
+          await arcapi_friend_clear(_arc_account);
+        } catch (e) { throw new APIError(-3, 'clear friend list failed'); }
 
-          // result of arcapi not include
-          // user code anymore since v6
-          _arc_friend = friends[0];
-          _arc_friend.code = argument.usercode;
-        })
-        .catch((error) => { reject(new APIError(-5, 'add friend failed')); });
+        // add friend
+        try {
+          _arc_friends = await arcapi_friend_add(_arc_account, argument.usercode);
+        } catch (e) { throw new APIError(-4, 'add friend failed'); }
+
+        // length must be 1
+        if (_arc_friends.length != 1)
+          throw new APIError(-5, 'internal error occurred');
+
+        // result of arcapi not include
+        // user code anymore since v6
+        _arc_friend = _arc_friends[0];
+        _arc_friend.code = argument.usercode;
+
+        // must do deep copy
+        const _return = JSON.parse(JSON.stringify(_arc_friend));
+        _return.recent_score = _arc_friend.recent_score[0];
+
+        // delete field if needn't recent data or not played yet
+        if (argument.hasrecent != 'true' || !_arc_friend.recent_score.length)
+          delete _return.recent_score;
+
+        resolve(_return);
+
+      } catch (e) {
+        // recycle account when any error occurred
+        if (_arc_account)
+          arcapi_account_release(_arc_account);
+        // re-throw the error
+        throw e;
+      }
 
       // release account
       arcapi_account_release(_arc_account)
         .catch((error) => { syslog.e(error.stack); });
-
       // update user info and recently played
       dbproc_arcplayer_update(_arc_friend)
         .catch((error) => { syslog.e(error.stack); });
-
       // insert new record into database
       if (_arc_friend.recent_score.length)
         dbproc_arcrecord_update(_arc_friend.user_id, _arc_friend.recent_score)
           .catch((error) => { syslog.e(error.stack); });
 
-      // fill the data template
-      const _return = _arc_friend;
-      _return.recent_score = _arc_friend.recent_score[0];
+    } catch (e) {
+      if (e instanceof APIError)
+        return reject(e);
 
-      // if need recent data
-      if (argument.hasrecent != 'true' && !_arc_friend.recent_score.length) {
-        delete _return.recent_score;
-      }
-
-      resolve(_return);
-
-    } catch (error) {
-
-      // release this account
-      if (_arc_account)
-        arcapi_account_release(_arc_account);
-
-      // only handle APIError
-      if (error instanceof APIError)
-        return reject(error);
-
-      syslog.e(TAG, error.stack);
-      reject(new APIError(-6, 'internal error'));
+      syslog.d(e.stack);
+      return reject(new APIError(-233, 'unknown error occurred'));
     }
   });
 }
