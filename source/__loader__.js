da524ff4f6762ecdd42b30ca0dda17463ee75b6a
CirnoBakaBOT <2-CirnoBakaBOT@users.noreply.git.moezsoft.com>
2020-04-19 01:19:36 +0800

    now supported favicon

--- source/__loader__.js
+++ source/__loader__.js
@@ -6,7 +6,8 @@
 
 const TAG = 'source/__loader__.js';
 
-module.exports = async (request, response) => {
+
+const handleMain = async (request, response) => {
 
   // process request url
   const _api_url = new URL(`http://0.0.0.0${request.url}`);
@@ -14,15 +15,55 @@ module.exports = async (request, response) => {
   const _api_arguments = Object.fromEntries(_api_url.searchParams);
   syslog.i(TAG, `Accept request at => ${_api_path} ${JSON.stringify(_api_arguments)}`);
 
+  // handle favicon request
+  if (_api_path == '/favicon.ico')
+    return handleIconRequest(response);
+
+  // handle general api request
+  return handleApiRequest(_api_path, _api_arguments, response);
+}
+
+const handleIconRequest = async (response) => {
+
+  let _http_body = null;
+  let _http_status = null;
+  let _http_content_type = null;
+
+  const file = require('fs');
+  try {
+    await file.promises.readFile('../favicon.ico')
+      .then((data) => {
+        _http_status = 200;
+        _http_body = data;
+        _http_content_type = 'image/x-icon';
+      });
+  } catch (e) {
+    syslog.e(e.stack);
+    _http_status = 404;
+    _http_body = '';
+    _http_content_type = '';
+  }
+
+  // send result to client
+  response.statusCode = _http_status;
+  response.setHeader('Content-Type', _http_content_type);
+  response.setHeader('Server', `BotArcAPI ${BOTARCAPI_VERSTR}`);
+  response.end(_http_body);
+  syslog.v(TAG, 'Send response back');
+}
+
+const handleApiRequest = async (path, argument, response) => {
+
   // make response body
   let _http_body = null;
   let _http_status = null;
+  let _http_content_type = null;
 
   try {
     // try invoke method
-    const _api_entry = require(`./publicapi/${_api_path}`);
+    const _api_entry = require(`./publicapi/${path}`);
     const _api_result = {};
-    await _api_entry(_api_arguments)
+    await _api_entry(argument)
       .then((result) => {
         _api_result.status = 0;
         _api_result.content = result;
@@ -32,23 +73,27 @@ module.exports = async (request, response) => {
         _api_result.message = error.notify;
       })
 
-    _http_body = JSON.stringify(_api_result);
     _http_status = 200;
+    _http_body = JSON.stringify(_api_result);
+    _http_content_type = 'application/json; charset=utf-8';
+
   }
   catch (e) {
-    syslog.e(TAG, e.toString());
     syslog.e(TAG, e.stack);
 
-    _http_body = 'request path notfound =(:3) z)_';
     _http_status = 404;
+    _http_body = 'request path notfound =(:3) z)_';
+    _http_content_type = 'text/html; charset=utf-8';
   }
 
-  // send json result to client
+  // send result to client
   response.statusCode = _http_status;
+  response.setHeader('Content-Type', _http_content_type);
   response.setHeader('Access-Control-Allow-Origin', '*');
-  response.setHeader('Content-Type', 'application/json; charset=utf-8');
   response.setHeader('Server', `BotArcAPI ${BOTARCAPI_VERSTR}`);
   response.end(_http_body);
 
   syslog.v(TAG, 'Send response back');
 }
+
+module.exports = handleMain;
