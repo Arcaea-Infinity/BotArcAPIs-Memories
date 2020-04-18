3a669165e559eeaf286e1fce63e0e5e0397e75b6
CirnoBakaBOT <2-CirnoBakaBOT@users.noreply.git.moezsoft.com>
2020-04-19 01:47:12 +0800

    updated public api checkupdate

--- source/publicapi/v1/checkupdate.js
+++ source/publicapi/v1/checkupdate.js
@@ -9,50 +9,46 @@ const fetch = require('node-fetch');
 const Request = require('node-fetch').Request;
 const APIError = require('../../corefunc/error');
 
-module.exports = async function () {
-  const _return_template = {
-    'status': 0,
-    'message': null,
-    'content': {
-      'version': null,
-      'download_link': null
+module.exports = () => {
+  return new Promise(async (resolve, reject) => {
+
+    try {
+
+      // construct remote request
+      const _remote_request =
+        new Request('https://www.fantasyroom.cn/api/v1/arcver.php', {
+          method: 'GET',
+          headers: {
+            'User-Agent': 'BotArcAPI'
+          }
+        });
+
+      // request remote service
+      fetch(_remote_request)
+        .then((response) => {
+          // parse json if success
+          if (response.status === 200)
+            return response.json();
+          throw new APIError(-1, 'remote service now is unavailable');
+        })
+        .then((root) => {
+          // check result if success
+          if (root.res === 'success') {
+            const _return = {};
+            _return.version = root.contents.android.version;
+            _return.download_link = root.contents.android.apk_dl_link;
+            return resolve(_return);
+          }
+          throw new APIError(-2, 'request remote service failed');
+        })
+        .catch((e) => { throw new APIError(-3, 'request timeout'); });
+
+    } catch (e) {
+      if (e instanceof APIError)
+        return reject(e);
+
+      syslog.e(TAG, e.stack);
+      return reject(new APIError(-233, 'unknown error occurred'));
     }
-  };
-
-  try {
-
-    // construct remote request
-    const _promise_remote =
-      new Request('https://www.fantasyroom.cn/api/v1/arcver.php', {
-        method: 'GET',
-        headers: {
-          'User-Agent': 'BotArcAPI'
-        }
-      });
-
-    // request remote service
-    await fetch(_promise_remote)
-      .then((response) => {
-        // parse json if success
-        if (response.status === 200)
-          return response.json();
-        throw new APIError(-1, 'remote service now is unavailable');
-      })
-      .then((jsonroot) => {
-        // check result if success
-        if (jsonroot.res === 'success') {
-          _return_template.content.version = jsonroot.contents.android.version;
-          _return_template.content.download_link = jsonroot.contents.android.apk_dl_link;
-        }
-        else throw new APIError(-2, 'request remote service failed');
-      })
-      .catch((e) => { throw new APIError(-3, 'request timeout'); });
-
-  } catch (e) {
-    syslog.e(TAG, e.notyify);
-    _return_template.status = e.status;
-    _return_template.message = e.notify;
-  }
-
-  return _return_template;
+  });
 };
