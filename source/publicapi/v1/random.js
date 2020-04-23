ef7151f94dcf7feca4343b63aa5f97ea8e86ac2c
CirnoBakaBOT <thesnowfield@moezsoft.com>
2020-04-24 01:06:25 +0800

    append new optional argument for random api

--- source/publicapi/v1/random.js
+++ source/publicapi/v1/random.js
@@ -7,13 +7,14 @@ const TAG = 'v1/random.js\t';
 
 const APIError = require('../../corefunc/error');
 const dbproc_arcsong_random = require('../../database/_dbproc_arcsong_random');
+const dbproc_arcsong_bysongid = require('../../database/_dbproc_arcsong_bysongid');
 
 module.exports = (argument) => {
   return new Promise(async (resolve, reject) => {
 
     try {
 
-      // /random?start=xx[&end=xx]
+      // /random?start=xx[&end=xx][&info=true]
       // check for request arguments
       argument.start = parseInt(argument.start);
       if (isNaN(argument.start)) argument.start = 0;
@@ -32,16 +33,62 @@ module.exports = (argument) => {
         throw new APIError(-2, 'invalid range of end');
 
       let _arc_song = null;
+      let _arc_songinfo = null;
+      let _return = {};
 
       // select song
       try {
         _arc_song = await dbproc_arcsong_random(argument.start, argument.end);
+        _return.id = _arc_song.sid;
+        _return.rating_class = _arc_song.rating_class;
       } catch (e) { throw new APIError(-3, 'internal error'); }
 
-      const _return = {
-        id: _arc_song.sid,
-        rating_class: _arc_song.rating_class
-      };
+      // return song info if need
+      if (argument.fullinfo == 'true') {
+        try {
+          _arc_songinfo = await dbproc_arcsong_bysongid(_arc_song.sid);
+          _return.song_info = {
+            id: _arc_songinfo.sid,
+            title_localized: {
+              en: _arc_songinfo.name_en,
+              ja: _arc_songinfo.name_jp
+            },
+            artist: _arc_songinfo.artist,
+            bpm: _arc_songinfo.bpm,
+            bpm_base: _arc_songinfo.bpm_base,
+            set: _arc_songinfo.pakset,
+            audioTimeSec: _arc_songinfo.time,
+            side: _arc_songinfo.side,
+            remote_dl: _arc_songinfo.remote_download == 'true' ? true : false,
+            world_unlock: _arc_songinfo.world_unlock == 'true' ? true : false,
+            date: _arc_songinfo.date,
+            difficulties: [
+              {
+                ratingClass: 0,
+                chartDesigner: _arc_songinfo.chart_designer_pst,
+                jacketDesigner: _arc_songinfo.jacket_designer_pst,
+                rating: _arc_songinfo.rating_pst / 10
+              },
+              {
+                ratingClass: 1,
+                chartDesigner: _arc_songinfo.chart_designer_prs,
+                jacketDesigner: _arc_songinfo.jacket_designer_prs,
+                rating: _arc_songinfo.rating_prs / 10
+              },
+              {
+                ratingClass: 2,
+                chartDesigner: _arc_songinfo.chart_designer_ftr,
+                jacketDesigner: _arc_songinfo.jacket_designer_ftr,
+                rating: _arc_songinfo.rating_ftr / 10
+              }
+            ]
+          };
+          // remove empty field
+          if (_return.song_info.title_localized.ja == "")
+            delete _return.song_info.title_localized.ja;
+
+        } catch (e) { throw new APIError(-4, 'internal error'); }
+      }
 
       resolve(_return);
 
