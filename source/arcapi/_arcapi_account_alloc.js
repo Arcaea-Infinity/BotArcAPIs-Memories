355b95c2faf77b79cc751ea077822ca3ed32a08f
CirnoBakaBOT <thesnowfield@moezsoft.com>
2020-04-26 14:02:39 +0800

    do auto login when token is null

--- source/arcapi/_arcapi_account_alloc.js
+++ source/arcapi/_arcapi_account_alloc.js
@@ -5,26 +5,70 @@
 
 const TAG = 'arcapi/_arcapi_account_alloc.js';
 
+const APIError = require('../corefunc/error');
+
+const arcapi_login = require('./_arcapi_login');
+const arcapi_userme = require('./_arcapi_userme');
+const dbproc_arcaccount_update = require('../database/_dbproc_arcaccount_update');
+
 module.exports = () => {
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
     // request an arc account from global ARCACCOUNT
     // ** pretend to be a queue =(:3) z)_ **
 
     if (typeof ARCACCOUNT == 'undefined')
       return reject(new Error('ARCACCOUNT is undefined?'));
-    if (!ARCACCOUNT.length)
-      return reject(new Error('ARCACCOUNT length is 0, no arc accounts'));
 
-    // grab an account from queue
-    const _element = ARCACCOUNT.shift(1);
-    if (typeof _element == 'undefined')
-      return reject(new Error('Element is undefined?'));
+    let _account = null;
 
-    resolve(_element);
-    syslog.i(TAG, `Allocated arc account => ${_element.name} ${_element.token}`);
-  })
+    while (true) {
+
+      if (!ARCACCOUNT.length)
+        return reject(new Error('ARCACCOUNT length is zero'));
+
+      // grab an account from queue
+      _account = ARCACCOUNT.shift(1);
+      if (typeof _account == 'undefined')
+        return reject(new Error('Element is undefined?'));
+
+      // if the account has no token
+      if (_account.token == '') {
+
+        // try login and request a new token
+        try {
+          _account.token = await arcapi_login(_account.name, _account.passwd, _account.device);
+        } catch (e) {
+
+          // this account has been banned
+          if (e instanceof APIError && e.status == 601) {
+            _account.banned = true;
+            syslog.w(TAG, `This account has been banned. remove from pool => ${_account.name}`);
+          }
+
+          syslog.e(TAG, e);
+
+        } finally {
+
+          // fetch information of account if needed
+          if (!_account.banned && (!_account.uid || !_account.ucode)) {
+            const _info = await arcapi_userme(_account);
+            _account.uid = _info.user_id;
+            _account.ucode = _info.user_code;
+          }
+
+          // update the database
+          dbproc_arcaccount_update(_account);
+
+          // available account
+          if (!_account.banned && _account.token != '')
+            break;
+
+        }
+
+      } else break;
+    }
+
+    resolve(_account);
+    syslog.i(TAG, `Allocated arc account => ${_account.name} ${_account.token}`);
+  });
 }
-
-
-
-
