8aeddb126f60900d1ffec60b2b5af4d1f60c3460
Moenya Zulandre Scarlet <1796245397@qq.com>
2020-07-08 11:14:17 +0800

    Update config.ts
    Upgrade ARCAPI_APPVERSION to 3.0.3c

--- source/corefunc/config.ts
+++ source/corefunc/config.ts
@@ -17,7 +17,7 @@ const _default_config: any = {
   // arcaea api config
   'ARCAPI_RETRY': 3,
   'ARCAPI_VERSION': 12,
-  'ARCAPI_APPVERSION': '3.0.2c',
+  'ARCAPI_APPVERSION': '3.0.3c',
   'ARCAPI_USERAGENT': 'Grievous Lady (Linux; U; Android 2.3.3; BotArcAPI)',
 
   // path to database folder
