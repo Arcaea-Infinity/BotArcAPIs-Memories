70dbd04a7fd37d914562a762835a7f90c4947e45
CirnoBakaBOT <thesnowfield@moezsoft.com>
2020-11-20 09:50:55 +0800

    upgrade to v3.2.3c

--- source/corefunc/config.ts
+++ source/corefunc/config.ts
@@ -6,8 +6,8 @@ const _default_config: any = {
   // botarcapi version
   'BOTARCAPI_MAJOR': 0,
   'BOTARCAPI_MINOR': 1,
-  'BOTARCAPI_VERSION': 3,
-  'BOTARCAPI_VERSTR': 'BotArcAPI v0.1.3',
+  'BOTARCAPI_VERSION': 4,
+  'BOTARCAPI_VERSTR': 'BotArcAPI v0.1.4',
 
   // useragent white list
   // if set '[]' will accept all requests
@@ -17,7 +17,7 @@ const _default_config: any = {
   // arcaea api config
   'ARCAPI_RETRY': 3,
   'ARCAPI_VERSION': 12,
-  'ARCAPI_APPVERSION': '3.2.2c',
+  'ARCAPI_APPVERSION': '3.2.3c',
   'ARCAPI_USERAGENT': 'Grievous Lady (Linux; U; Android 2.3.3; BotArcAPI)',
 
   // path to database folder
