5e3b6f7bd21ff1797bf7856ef19faaeb3dea71c3
CirnoBakaBOT <thesnowfield@moezsoft.com>
2021-04-17 10:55:46 +0800

    batch api

---
+++ source/publicapi/v4/batch.ts
@@ -0,0 +1,12 @@
+const TAG: string = 'v4/batch.ts\t';
+
+import syslog from '../../modules/syslog/syslog';
+import APIError from '../../modules/apierror/apierror';
+
+export default (argument: any): Promise<any> => {
+
+  return new Promise(async (resolve, reject) => {
+    reject();
+  });
+
+}
