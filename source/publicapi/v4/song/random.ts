26724c3a321875d839979f6ad9ab140d0d4e115e
CirnoBakaBOT <thesnowfield@moezsoft.com>
2021-04-19 20:14:18 +0800

    remove useless params

--- source/publicapi/v4/song/random.ts
+++ source/publicapi/v4/song/random.ts
@@ -3,8 +3,6 @@ const TAG: string = 'v4/song/random.ts\t';
 import syslog from  '../../../modules/syslog/syslog';
 import APIError from  '../../../modules/apierror/apierror';
 import arcsong_random from  '../../../modules/database/database.arcsong.byrand';
-import arcsong_bysongid from  '../../../modules/database/database.arcsong.bysongid';
-import IDatabaseArcSong from  '../../../modules/database/interfaces/IDatabaseArcSong';
 
 export default (argument: any): Promise<any> => {
 
@@ -12,7 +10,7 @@ export default (argument: any): Promise<any> => {
 
     try {
 
-      // /random?start=xx[&end=xx][&info=true]
+      // /song/random?start=xx[&end=xx]
       // check for request arguments
       argument.start = parseInt(argument.start);
       if (isNaN(argument.start)) argument.start = 0;
@@ -32,7 +30,6 @@ export default (argument: any): Promise<any> => {
         throw new APIError(-2, 'invalid range of end');
 
       let _arc_song: any = null;
-      let _arc_songinfo: IDatabaseArcSong = <IDatabaseArcSong>{};
       let _return: any = {};
 
       // select song
@@ -41,86 +38,6 @@ export default (argument: any): Promise<any> => {
         _return.id = _arc_song.sid;
         _return.rating_class = _arc_song.rating_class;
       } catch (e) { throw new APIError(-3, 'internal error'); }
-
-      // return song info if needed
-      if (argument.info == 'true') {
-
-        try {
-          _arc_songinfo = await arcsong_bysongid(_arc_song.sid);
-          _return.song_info = {
-            id: _arc_songinfo.sid,
-            title_localized: {
-              en: _arc_songinfo.name_en,
-              ja: _arc_songinfo.name_jp
-            },
-            artist: _arc_songinfo.artist,
-            bpm: _arc_songinfo.bpm,
-            bpm_base: _arc_songinfo.bpm_base,
-            set: _arc_songinfo.pakset,
-            audioTimeSec: _arc_songinfo.time,
-            side: _arc_songinfo.side,
-            remote_dl: _arc_songinfo.remote_download == 'true' ? true : false,
-            world_unlock: _arc_songinfo.world_unlock == 'true' ? true : false,
-            date: _arc_songinfo.date,
-            difficulties: [
-              {
-                ratingClass: 0,
-                chartDesigner: _arc_songinfo.chart_designer_pst,
-                jacketDesigner: _arc_songinfo.jacket_designer_pst,
-                rating: _arc_songinfo.difficultly_pst,
-                ratingReal: _arc_songinfo.rating_pst,
-                ratingPlus: (_arc_songinfo.difficultly_pst % 2 != 0),
-                totalNotes: _arc_songinfo.notes_pst
-              },
-              {
-                ratingClass: 1,
-                chartDesigner: _arc_songinfo.chart_designer_prs,
-                jacketDesigner: _arc_songinfo.jacket_designer_prs,
-                rating: _arc_songinfo.difficultly_prs,
-                ratingReal: _arc_songinfo.rating_prs,
-                ratingPlus: (_arc_songinfo.difficultly_prs % 2 != 0),
-                totalNotes: _arc_songinfo.notes_prs
-              },
-              {
-                ratingClass: 2,
-                chartDesigner: _arc_songinfo.chart_designer_ftr,
-                jacketDesigner: _arc_songinfo.jacket_designer_ftr,
-                rating: _arc_songinfo.difficultly_ftr,
-                ratingReal: _arc_songinfo.rating_ftr,
-                ratingPlus: (_arc_songinfo.difficultly_ftr % 2 != 0),
-                totalNotes: _arc_songinfo.notes_ftr
-              }
-            ]
-          };
-
-          // append beyond rating
-          if (_arc_songinfo.difficultly_byn != -1) {
-            _return.song_info.difficulties[3] = {
-              ratingClass: 3,
-              chartDesigner: _arc_songinfo.chart_designer_byn,
-              jacketDesigner: _arc_songinfo.jacket_designer_byn,
-              rating: _arc_songinfo.difficultly_byn,
-              ratingReal: _arc_songinfo.rating_byn,
-              ratingPlus: (_arc_songinfo.difficultly_byn % 2 != 0),
-              totalNotes: _arc_songinfo.notes_byn
-            };
-          }
-
-          // append rating
-          _return.song_info.difficulties.map((element: any) => {
-            element.rating = Math.floor(element.rating / 2);
-            if (!element.ratingPlus)
-              delete element.ratingPlus;
-            return element;
-          });
-
-          // remove empty field
-          if (_return.song_info.title_localized.ja == '')
-            delete _return.song_info.title_localized.ja;
-
-        } catch (e) { throw new APIError(-4, 'internal error'); }
-
-      }
 
       resolve(_return);
 
