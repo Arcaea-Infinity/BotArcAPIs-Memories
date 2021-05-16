5949515a85f427c9fe9f16f88ad730fe0c7a9e96
CirnoBakaBOT <thesnowfield@moezsoft.com>
2021-05-16 14:12:07 +0800

    0.3.3

--- source/corefunc/config.ts
+++ source/corefunc/config.ts
@@ -6,8 +6,8 @@ const _default_config: any = {
   // botarcapi version
   'BOTARCAPI_MAJOR': 0,
   'BOTARCAPI_MINOR': 3,
-  'BOTARCAPI_VERSION': 2,
-  'BOTARCAPI_VERSTR': 'BotArcAPI v0.3.2',
+  'BOTARCAPI_VERSION': 3,
+  'BOTARCAPI_VERSTR': 'BotArcAPI v0.3.3',
 
   // useragent whitelist
   // if set '[]' will accept all requests
